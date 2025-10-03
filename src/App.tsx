26fc163e4298ee4817f43bb15fa2c99bf5696890
hee-suh <suhee0206@gmail.com>
2025-10-03 16:14:02 +0900

    feat: add shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { Toaster } from '@/components/ui/sonner';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
+      <Toaster />
       <Outlet />
     </AuthProvider>
   </QueryClientProvider>
