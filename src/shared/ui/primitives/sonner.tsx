cec85c17deabd9b15dd3a45df0be459dd0804813
hee-suh <suhee0206@gmail.com>
2025-11-13 00:26:50 +0900

    feat: update toast style

--- src/shared/ui/primitives/sonner.tsx
+++ src/shared/ui/primitives/sonner.tsx
@@ -1,25 +1,43 @@
 import { useTheme } from 'next-themes';
 import { Toaster as Sonner } from 'sonner';
 
+import { Icon } from '@/shared/icons/icon';
+
 import type { ToasterProps } from 'sonner';
 
-const Toaster = ({ ...props }: ToasterProps) => {
+export function Toaster(props: ToasterProps) {
   const { theme = 'system' } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
-      className='toaster group'
-      style={
-        {
-          '--normal-bg': 'var(--popover)',
-          '--normal-text': 'var(--popover-foreground)',
-          '--normal-border': 'var(--border)'
-        } as React.CSSProperties
-      }
+      icons={{
+        success: (
+          <Icon
+            name='circle_check_on'
+            size={22}
+            color='currentColor'
+            className='text-runddy-green'
+          />
+        ),
+        error: (
+          <Icon
+            name='warning'
+            size={22}
+            color='currentColor'
+            className='text-state-error'
+          />
+        )
+      }}
+      toastOptions={{
+        unstyled: true,
+        classNames: {
+          toast:
+            'rounded-full bg-g-80 text-white shadow-runddy px-5 py-4 flex items-center gap-2.5',
+          title: 'text-contents-m15'
+        }
+      }}
       {...props}
     />
   );
-};
-
-export { Toaster };
+}
