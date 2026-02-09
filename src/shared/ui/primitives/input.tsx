e6f4163a475ed98e9448e8f63fa2fae3420be9d6
hee-suh <suhee0206@gmail.com>
2026-02-09 20:56:19 +0900

    fix: update input css

--- src/shared/ui/primitives/input.tsx
+++ src/shared/ui/primitives/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot='input'
       className={cn(
-        'placeholder:text-ter text-g-100 text-contents-m16 selection:bg-primary bg-g-10 file:text-contents-r15 md:text-contents-r15 flex h-14 w-full min-w-0 rounded-xl p-4 transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:bg-transparent disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        'placeholder:text-ter text-g-100 text-contents-r16 selection:bg-primary bg-g-10 file:text-contents-r15 md:text-contents-r15 flex h-14 w-full min-w-0 rounded-xl p-4 transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:bg-transparent disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
         'hover:bg-g-200 active:bg-g-200 focus-visible:bg-g-20 transition-colors',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
         className
